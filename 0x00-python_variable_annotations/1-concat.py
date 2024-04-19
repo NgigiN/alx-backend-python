28b402ac1018541f8a8f317e122352f365f14977
NgigiN <ngigi.nyongo@gmail.com>
2024-04-19 18:15:10 +0300

    Basic string concat function annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+  This module contains a type-annotated function
+  that takes 2 string arguments  and returns a concatenated
+  string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This is my type annotated string concatenation
+    function
+    """
+    return str1 + str2
