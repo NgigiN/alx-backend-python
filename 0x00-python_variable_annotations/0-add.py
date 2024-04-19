26676bc268a4cf27e98b2e468f624e4463fa4c7b
NgigiN <ngigi.nyongo@gmail.com>
2024-04-19 18:15:10 +0300

    Basic function annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module demonstrates a type annotated function
+It takes float values and returns float values"""
+
+
+def add(a: float, b: float) -> float:
+    """function correctly annotating values"""
+    return a + b
