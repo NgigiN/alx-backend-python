efd17256cbfcad9983f2ae88c4dd0c2af216c705
NgigiN <ngigi.nyongo@gmail.com>
2024-04-19 18:25:06 +0300

    Created a type annotated floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+  This module contains a type-annotated floor function
+  which should take a float adn return the floor of that
+  float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This is my type-annotated rounding down function
+    """
+    return int(n)
