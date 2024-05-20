60b179609eabd1c19b0bfe28d4fc04c0eca24a1d
NgigiN <ngigi.nyongo@gmail.com>
2024-05-20 11:25:55 +0300

    measuring the running time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+async def measure_time(n: int = 0, max_delay: int = 10) -> float:
     """A function that takes integer arguments measures
     total execution time for wait_n( and returns total_time/n
     """
