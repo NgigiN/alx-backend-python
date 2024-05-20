a302d7c7a9530cc9bf9f3ef6016f87370fa030e9
NgigiN <ngigi.nyongo@gmail.com>
2024-05-20 10:26:11 +0300

    Module executes multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """Module that executes multiple coroutines at the same time
 with async
 """
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that executes the multiple coroutines at the same time
     with async
     """
