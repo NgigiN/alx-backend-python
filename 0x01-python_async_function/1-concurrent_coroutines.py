fe4b3b679ffa981d1a63e889a85c027c6b67f7bb
NgigiN <ngigi.nyongo@gmail.com>
2024-05-20 11:08:20 +0300

    Module executes multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,7 @@
 with async
 """
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -10,5 +11,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that executes the multiple coroutines at the same time
     with async
     """
-    delay_list = [await wait_random(max_delay) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delay_list = [await task for task in asyncio.as_completed(tasks)]
     return delay_list
